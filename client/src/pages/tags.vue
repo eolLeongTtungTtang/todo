d29cb41cd331e5449b10f08eba2ef2620a73a595
Minji287 <dpa357@gmail.com>
2025-01-29 14:50:52 +0900

    [FE] feat(태그별 화면): 화면 UI 구현

--- client/src/pages/tags.vue
+++ client/src/pages/tags.vue
@@ -2,8 +2,8 @@
   <v-expansion-panels flat v-model="panel">
     <v-expansion-panel v-for="(title, index) in tagArr" :key="index">
       <v-expansion-panel-title>
-        <span class="titleText"># {{ title }}</span>
-        <span class="greyText">&nbsp;&nbsp;+{{ index }}</span>
+        <span class="titleText"># {{ title.tag }}</span>
+        <span class="greyText">&nbsp;&nbsp;+{{ title.maxTodoId }}</span>
         <template v-slot:actions="{ expanded }">
           <span class="toggleText">{{ expanded ? "닫기" : "펼치기" }}</span>
         </template>
@@ -12,6 +12,7 @@
       <v-expansion-panel-text class="panelText">
         <base-todolist />
       </v-expansion-panel-text>
+
       <base-divider
         v-if="tagArr.length - 1 !== index"
         thickness="1"
@@ -30,10 +31,22 @@ import todoByTagTest from "../data/todoByTagTest.json";
 let panel = ref(null);
 
 // 태그
-const tagArr = Object.keys(todoByTagTest);
+const tagArr = Object.entries(todoByTagTest).map(([tag, todoList]) => {
+  // reduce를 사용하여 최대 todo_id 값을 찾기
+  const maxTodoId = todoList.reduce(
+    (max, todo) => Math.max(max, todo.todo_id),
+    -Infinity
+  );
+
+  return { tag, maxTodoId };
+});
 </script>
 
 <style scoped>
+::v-deep .v-expansion-panel-text__wrapper {
+  padding: 0 !important;
+}
+
 .titleText {
   font-weight: bold;
   font-size: 16px;
@@ -41,6 +54,7 @@ const tagArr = Object.keys(todoByTagTest);
 
 .panelText {
   font-weight: normal;
+  padding: 0 !important;
 }
 
 .toggleText {
